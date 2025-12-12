e3a9596ad72c868a373dc384fede13b26e97634c
JT <jttutor10@gmail.com>
2025-12-12 11:11:27 -0600

    room creation

--- videochats/views.py
+++ videochats/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 @login_required
@@ -9,8 +10,19 @@ def lobby_view(request):
 
 @login_required
 def create_room_view(request):
-    return redirect('lobby')
-
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            # we save the form but dont commit to the DB so we can add the host, 
+            # and THEN add it to the DB
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
+            return redirect('room', room_id=room.room_id)
+    else:
+        form = RoomForm()
+    
+    return render(request, 'videochats/create_room.html', {'form': form})
 @login_required
 def room_view(request, room_id):
     try:
